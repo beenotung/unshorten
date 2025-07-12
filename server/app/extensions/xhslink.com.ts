b8daa7f009526f4a52624cbf66d467fd3327a270
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-07-12 14:13:31 +0800

    patch: support xhs link using space (previous using comma)

--- server/app/extensions/xhslink.com.ts
+++ server/app/extensions/xhslink.com.ts
@@ -2,14 +2,24 @@ import { removeTrackingParamsExcept } from '../untrack.js'
 import { addLinkMiddleware } from './index.js'
 
 addLinkMiddleware(link => {
-  let start = link.indexOf('http://xhslink.com/a/')
+  let start_1 = link.indexOf('http://xhslink.com/a/')
+  let start_2 = link.indexOf('http://xhslink.com/m/')
+  let start = pickIndex(start_1, start_2)
   if (start == -1) return link
 
-  let end = link.indexOf('，', start)
+  let end_1 = link.indexOf('，', start)
+  let end_2 = link.indexOf(' ', start)
+  let end = pickIndex(end_1, end_2)
   if (end == -1) return link
 
   return link.slice(start, end)
 })
+
+function pickIndex(a: number, b: number) {
+  if (a == -1) return b
+  if (b == -1) return a
+  return a < b ? a : b
+}
 
 export function removeXhsTrackingParams(context: {
   removedTrackingParams: Record<string, string[]>
