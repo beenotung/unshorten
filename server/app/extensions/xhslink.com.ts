ac5e4b08ccbd538e57bb7a7e683b503ca8bda564
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-09-28 08:55:22 +0800

    patch: support xhs link using newline

--- server/app/extensions/xhslink.com.ts
+++ server/app/extensions/xhslink.com.ts
@@ -4,21 +4,22 @@ import { addLinkMiddleware } from './index.js'
 addLinkMiddleware(link => {
   let start_1 = link.indexOf('http://xhslink.com/a/')
   let start_2 = link.indexOf('http://xhslink.com/m/')
-  let start = pickIndex(start_1, start_2)
+  let start_3 = link.indexOf('http://xhslink.com/o/')
+  let start = pickIndex(start_1, start_2, start_3)
   if (start == -1) return link
 
   let end_1 = link.indexOf('，', start)
   let end_2 = link.indexOf(' ', start)
-  let end = pickIndex(end_1, end_2)
+  let end_3 = link.indexOf('\n', start)
+  let end = pickIndex(end_1, end_2, end_3)
   if (end == -1) return link
 
   return link.slice(start, end)
 })
 
-function pickIndex(a: number, b: number) {
-  if (a == -1) return b
-  if (b == -1) return a
-  return a < b ? a : b
+function pickIndex(...args: number[]) {
+  if (args.length === 0) return -1
+  return Math.min(...args.filter(a => a !== -1))
 }
 
 export function removeXhsTrackingParams(context: {
